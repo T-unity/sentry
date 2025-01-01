2edde2d1c9afa99a0c1b36cda834f92af076df92
T-unity <takadanaritoshi@gmail.com>
2025-01-01 14:28:04 +0900

    make stack

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-		"log"
-		"time"
+    "errors"
+    "fmt"
+    "log"
+    "math/rand"
+    "time"
 
-		"github.com/getsentry/sentry-go"
+    "github.com/getsentry/sentry-go"
 )
 
 func main() {
-		err := sentry.Init(sentry.ClientOptions{
-			Dsn: "https://5446d1adb4536d230c2164b75b9de107@o4508539131265024.ingest.us.sentry.io/4508539236319232",
-			// Set TracesSampleRate to 1.0 to capture 100%
-			// of transactions for tracing.
-			// We recommend adjusting this value in production,
-			TracesSampleRate: 1.0,
-		})
-		if err != nil {
-			log.Fatalf("sentry.Init: %s", err)
-		}
-		// Flush buffered events before the program terminates.
-		defer sentry.Flush(2 * time.Second)
+    err := sentry.Init(sentry.ClientOptions{
+        Dsn:              "https://5446d1adb4536d230c2164b75b9de107@o4508539131265024.ingest.us.sentry.io/4508539236319232",
+        TracesSampleRate: 1.0,
+    })
+    if err != nil {
+        log.Fatalf("sentry.Init: %s", err)
+    }
+    defer sentry.Flush(2 * time.Second)
 
-		sentry.CaptureMessage("Oooooooops!!")
+    if err := foo(); err != nil {
+        randStr := randomString(8)
+        sentry.WithScope(func(scope *sentry.Scope) {
+            scope.SetFingerprint([]string{"my-unique-error", randStr})
+            sentry.CaptureException(err)
+        })
+    }
+}
+
+func foo() error {
+    return bar()
+}
+
+func bar() error {
+    return baz()
+}
+
+func baz() error {
+    now := time.Now().Format(time.RFC3339)
+    r := randomString(8)
+    return errors.New(fmt.Sprintf("%s_%s", r, now))
+}
+
+func randomString(n int) string {
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    rand.Seed(time.Now().UnixNano())
+    b := make([]byte, n)
+    for i := 0; i < n; i++ {
+        b[i] = letters[rand.Intn(len(letters))]
+    }
+    return string(b)
 }
